b273d1fd4f2151fdd7c9cdc9040d82f58ec7cac6
HariyantoDotx <hari@dedicated-it.com>
2022-03-07 21:43:20 +0800

    change port to 5050

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,24 +1,24 @@
-import axios from "axios"
+import axios from 'axios'
 
-const apiURL = ["https://memories-pritam.herokuapp.com", "http://localhost:5000"]
-const API = axios.create({ baseURL: apiURL[0] })
+const apiURL = ['https://memories-pritam.herokuapp.com', 'http://localhost:5050']
+const API = axios.create({ baseURL: apiURL[1] })
 
-API.interceptors.request.use(req => {
-    if (localStorage.getItem("profile")) {
-        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`
-    }
-    return req
+API.interceptors.request.use((req) => {
+	if (localStorage.getItem('profile')) {
+		req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+	}
+	return req
 })
 
-export const fetchPosts = page => API.get(`/posts?page=${page}`)
-export const fetchPost = id => API.get(`/posts/${id}`)
-export const fetchPostsBySearch = searchQuery => API.get(`/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${searchQuery.tags || "none"}`)
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`)
+export const fetchPost = (id) => API.get(`/posts/${id}`)
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags || 'none'}`)
 
-export const createPost = newPost => API.post("/posts", newPost)
+export const createPost = (newPost) => API.post('/posts', newPost)
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost)
-export const deletePost = id => API.delete(`/posts/${id}`)
-export const likePost = id => API.patch(`/posts/${id}/likePost`)
+export const deletePost = (id) => API.delete(`/posts/${id}`)
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`)
 export const comment = (value, id) => API.post(`/posts/${id}/commentPost`, { value })
 
-export const signIn = formData => API.post("/user/signin", formData)
-export const signUp = formData => API.post("/user/signup", formData)
+export const signIn = (formData) => API.post('/user/signin', formData)
+export const signUp = (formData) => API.post('/user/signup', formData)
